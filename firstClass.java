743260deae4a39b078db8c2285a448dc43b44d19
Роман Лисейкин <ifoxer@MacBook-Air-Roman.local>
2019-12-03 01:59:28 +0300

    second commit

---
+++ firstClass.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class firstClass {
+
+    /**
+     * Метод main складывает дробные числа,результат округляет до 4х знаков после запятой.
+     */
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите первое значение: ");
+        float first = scn.nextFloat();
+        System.out.println("Введите второе значение: ");
+        float second = scn.nextFloat();
+        float res = first + second;
+        System.out.printf ("%.4f", res);
+    }
+}
