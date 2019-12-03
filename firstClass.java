5897c269de0fd5d6ee753b191d6f53e1668483e1
ifoxer <romanliseykin@gmail.com>
2019-12-03 20:49:31 +0300

    Задания 1,2,3

--- firstClass.java
+++ firstClass.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class firstClass {
 
     /**
-     * Метод main складывает дробные числа,результат округляет до 4х знаков после запятой.
+     * Метод main выполняет операции (+,-,*,/) для чисел с плавающей точкой, результат округляет до 4х знаков после запятой.
      */
 
     public static void main(String[] args) {
@@ -12,8 +12,71 @@ public class firstClass {
         float first = scn.nextFloat();
         System.out.println("Введите второе значение: ");
         float second = scn.nextFloat();
+/**
+ * switch для выбора операции
+ */
+        System.out.println("Введите желаемую операцию: ");
+        String oper = scn.next();
+
+        switch (oper) {
+            case "-":
+                v(first,second);
+                break;
+
+            case "+":
+                u(first,second);
+                break;
+
+            case "*":
+                u(first,second);
+                break;
+
+            case "/":
+                d(first,second);
+                break;
+
+        }
+
+    }
+
+    /**
+     *
+     * @return сложение дробных чисел
+     */
+    public static float s (float first, float second) {
         float res = first + second;
         System.out.printf ("%.4f", res);
+        return res;
+    }
+
+    /**
+     *
+     * @return вычитание дробных чисел
+     */
+    public static float v (float first, float second){
+        float res = first - second;
+        System.out.printf ("%.4f", res);
+        return res;
+    }
+
+    /**
+     *
+     * @return умножение дробных чисел
+     */
+    public static float u (float first, float second){
+        float res = first * second;
+        System.out.printf ("%.4f", res);
+        return res;
+    }
+
+    /**
+     *
+     * @return деление дробных чисел
+     */
+    public static float d (float first, float second){
+        float res = first / second;
+        System.out.printf ("%.4f", res);
+        return res;
     }
 }
 
